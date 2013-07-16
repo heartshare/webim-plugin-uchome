2ebf4e550489faaecb8b5cc07a07d822c0eb0196
Arron <arronzhang@me.com>
2013-07-16 16:57:45 +0800

    Fix time type

--- admin/schema.php
+++ admin/schema.php
@@ -21,8 +21,8 @@ $im_queries = "CREATE TABLE $imdb->webim_histories (
 	`timestamp` double DEFAULT NULL,
 	`todel` tinyint(1) NOT NULL DEFAULT '0',
 	`fromdel` tinyint(1) NOT NULL DEFAULT '0',
-	`created_at` date DEFAULT NULL,
-	`updated_at` date DEFAULT NULL,
+	`created_at` timestamp NULL DEFAULT NULL,
+	`updated_at` timestamp NULL DEFAULT NULL,
 	PRIMARY KEY (`id`),
 	KEY `todel` (`todel`),
 	KEY `fromdel` (`fromdel`),
@@ -36,8 +36,8 @@ CREATE TABLE $imdb->webim_settings (
 	`uid` mediumint(8) unsigned NOT NULL,
 	`web` blob,
 	`air` blob,
-	`created_at` date DEFAULT NULL,
-	`updated_at` date DEFAULT NULL,
+	`created_at` timestamp NULL DEFAULT NULL,
+	`updated_at` timestamp NULL DEFAULT NULL,
 	PRIMARY KEY (`id`) 
 ) ENGINE=MyISAM $charset_collate;";
 
